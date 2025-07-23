4170a1e25c6972f4e3bbf1adf0fed7b8700ab379
Pirishita <pirishita2005@gmail.com>
2025-07-23 06:57:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+!pip install plotly
+import plotly
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
